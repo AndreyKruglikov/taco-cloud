eacf03b4427f9be1f8bb17b3fa0710ef1469f0fa
Nemo <andreykruglikov@yandex.ru>
2019-10-27 15:46:19 +0300

    add Taco class

---
+++ src/main/java/com/kroogle/tacocloud/model/Taco.java
@@ -0,0 +1,25 @@
+package com.kroogle.tacocloud.model;
+
+import java.util.List;
+
+public class Taco {
+
+    private String name;
+    private List<String> ingredients;
+
+    public Taco() {
+    }
+
+    public Taco(String name, List<String> ingredients) {
+        this.name = name;
+        this.ingredients = ingredients;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<String> getIngredients() {
+        return ingredients;
+    }
+}
